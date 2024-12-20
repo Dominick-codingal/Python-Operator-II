672570307ca3a10fbdaf68c8556b8d198678685c
Dominick-codingal <dominickcodingal@gmail.com>
2024-12-20 04:09:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 6
+if a and b and c:
+   print("All numbers have boolean vaues as true")
+else:
+   print("Atleast one number has boolean value as false")
+a = 5
+b = 0
+c = -68
+if a > 0 or b > 0:
+   print("either of the numer is greater than o")
+else:
+   print("non of the number is greator than 0 ")
+if b >0 or c > 0:
+   print("either of the number is greator than 0")
+else:
+   print("no number is greator than 0")
---
+++ main1.py
@@ -0,0 +1,11 @@
+x = 56
+y = 6
+z = 6
+print (x != y)
+print (y != z)
+if x != y :
+    print( x, 'and' y, are different)
+
+a = int(input("entera number"))
+if a%2 !=0 :
+   print (a,"is not even number")
